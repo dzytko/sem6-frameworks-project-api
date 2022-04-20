ff1f34c1e4ad5b61c70819430dace51b16e67e90
dzytko <dawid.zytko@gmail.com>
2022-04-20 18:04:07 +0200

    fix: add categories to seeder

--- data/seeder.js
+++ data/seeder.js
@@ -1,9 +1,11 @@
 const {seedUsers} = require("./seed_users");
 const {seedProducts} = require("./seed_products");
+const {seedCategories} = require("./seed_categories");
 
 async function seedDb() {
     await seedUsers();
     await seedProducts();
+    await seedCategories()
 }
 
 module.exports = {seedDb}
