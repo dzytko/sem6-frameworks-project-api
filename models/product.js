b59f6bd015c002fcb332a8f0f3afd0b4b40df571
dzytko <dawid.zytko@gmail.com>
2022-04-20 17:26:28 +0200

    fix: change categoryId, discountedUnitPrice, properties, imagePath to nullable

--- models/product.js
+++ models/product.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema({
     productName: {type: String, required: true, unique: true},
-    categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
+    categoryId: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: false},
     unitPrice: {type: Number, required: true},
     isDiscounted: {type: Boolean, required: true},
     isDiscontinued: {type: Boolean, required: true},
-    discountedUnitPrice: {type: Number, required: true},
-    properties: {type: mongoose.Schema.Types.Map, required: true},
-    imagePath: {type: String, required: true},
+    discountedUnitPrice: {type: Number, required: false},
+    properties: {type: mongoose.Schema.Types.Map, required: false},
+    imagePath: {type: String, required: false},
     quantity: {type: Number, required: true},
 })
 
