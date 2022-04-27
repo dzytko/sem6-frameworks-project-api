1d70c16fbaa16f7a24afb3aa4c4f7c5bcd5cc60d
dzytko <dawid.zytko@gmail.com>
2022-04-27 16:17:06 +0200

    feat(user): add patch endpoint

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,8 @@ router.get('/', authenticate, (req, res) => {
         if (err) {
             console.log(err)
             res.status(500).send('Internal server error')
-        } else {
+        }
+        else {
             res.send(users)
         }
     })
@@ -47,7 +48,68 @@ router.post('/', async (req, res) => {
     })
 })
 
-router.delete('/:id', authenticate, (req, res) => {
+router.patch('/', authenticate, async (req, res) => {
+    // #swagger.tags = ["User"]
+    // #swagger.summary = "Update current user"
+
+    if (req.user.email && req.user.email !== req.body.email) {
+        const user = await User.findOne({email: req.user.email, _id: {$ne: req.user._id}})
+        if (user) {
+            return res.status(409).send({message: 'User with this email already exists'})
+        }
+    }
+
+    const user = await User.findOne({_id: req.user._id})
+    if (!user) {
+        return res.status(404).send('User not found')
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) {
+        return res.status(400).send({message: 'Invalid password'})
+    }
+
+    if (!user) {
+        return
+    }
+
+    user.firstName = req.body.firstName ?? user.firstName
+    user.lastName = req.body.lastName ?? user.lastName
+    user.email = req.body.email ?? user.email
+    const oldPasswordHashed = user.password
+    user.password = req.body.newPassword ?? 'StrongDummyPassword420#'
+
+    const {error} = validateUser(user)
+    if (error) {
+        return res.status(400).send({message: error.details[0].message})
+    }
+
+    if (req.body.newPassword) {
+        const salt = await bcrypt.genSalt(Number(process.env.SALT_ROUNDS))
+        user.password = await bcrypt.hash(req.body.newPassword, salt)
+    }
+    else {
+        user.password = oldPasswordHashed
+    }
+
+    await user.save((err, user) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send('Internal server error')
+        }
+        res.send({
+            message: 'User updated', user:
+                {
+                    _id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                },
+        })
+    })
+})
+
+router.delete('/', authenticate, (req, res) => {
     // #swagger.tags = ["User"]
     // #swagger.summary = "Delete user"
     User.findByIdAndDelete(req.params.id, (err, user) => {
