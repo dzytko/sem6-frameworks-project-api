8c87f18d9423a619da37c06c68e630f0a7ef82d5
dzytko <dawid.zytko@gmail.com>
2022-05-11 13:56:17 +0200

    feat: update category seeder

--- data/seed_categories.js
+++ data/seed_categories.js
@@ -9,12 +9,29 @@ async function seedCategories() {
             parentId: null,
         },
         {
+            _id: "00112233445566778899aabc",
             categoryName: "category2",
             parentId: "00112233445566778899aabb",
         },
         {
+            _id: "00112233445566778899aabd",
             categoryName: "category3",
             parentId: "00112233445566778899aabb",
+        },
+        {
+            _id: "00112233445566778899aabe",
+            categoryName: "category31",
+            parentId: "00112233445566778899aabd",
+        },
+        {
+            _id: "00112233445566778899aabf",
+            categoryName: "category32",
+            parentId: "00112233445566778899aabd",
+        },
+        {
+            _id: "00112233445566778899aaa0",
+            categoryName: "category321",
+            parentId: "00112233445566778899aabf",
         },
     ]
     await Category.insertMany(categories);
